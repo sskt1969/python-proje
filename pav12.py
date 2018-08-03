650d7d18382c8791980f11f87602a79189ec20b0
sskt1969 <41769896+sskt1969@users.noreply.github.com>
2018-08-03 15:19:36 +0530

    Create pav12.py

---
+++ pav12.py
@@ -0,0 +1,14 @@
+number = int(input())
+
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print("yes")
+            break
+    else:
+        print("yes")
+
+
+else:
+    print("no")
