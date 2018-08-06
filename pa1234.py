4acdd0560ac6738282c2b06fc9c0aa70ef26fa42
sskt1969 <41769896+sskt1969@users.noreply.github.com>
2018-08-06 14:02:02 +0530

    Rename pava1234.py to pa1234.py

---
+++ pa1234.py
@@ -0,0 +1,13 @@
+def largest(arr,n):
+  max = arr[0]
+  for i in range(1, n):
+    if arr[i] > max:
+            max = arr[i]
+  return max
+ 
+
+arr = int(input())
+n = int(input())
+Ans = largest(arr,n)
+print (Ans)
+ 
